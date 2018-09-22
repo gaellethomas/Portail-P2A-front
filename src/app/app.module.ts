bc7b9c05bc72612324b63ba65e62cd3f15c7e9a6
unknown <gaelle.thomas.pf1@gmail.com>
2018-09-22 04:00:05 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from 'src/app/footer/footer.component';
+import { BodyComponent } from 'src/app/body/body.component';
+import { HeaderComponent } from 'src/app/header/header.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
