87e8d525d1b1bc65a0c8aca7c812912db94a8ae6
unknown <gaelle.thomas.pf1@gmail.com>
2018-09-24 12:03:34 +0200

    second comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,45 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from 'src/app/footer/footer.component';
 import { BodyComponent } from 'src/app/body/body.component';
 import { HeaderComponent } from 'src/app/header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { SearchComponent } from './search/search.component';
+import { CreateComponent } from './create/create.component';
+import { UpdateComponent } from './update/update.component';
 
+const routes: Routes = [
+
+  // path = ' ' indicate if there is nothing after the port in the URL, the router redirect to path/accueil
+  // and display "Accueil" sheet by default
+  // path = "**" : if there is anything, never mind, the router display "Aliment" sheet
+
+    { path: '', redirectTo: '/accueil', pathMatch: 'full' },
+
+    /*{
+     path: 'commun',
+      component: CommunComponent},
+
+    {
+      path: 'activity/blueprism', 'activity/contextor', 'activity/web','activity/autres-assets', 'activity/suivi-de-production',
+      component: ActivityComponent},*/
+
+    {
+        path: 'recherche',
+         component: SearchComponent},
+
+    {
+    path: 'creer',
+      component: CreateComponent},
+
+    {
+    path: 'mise-a-jour',
+      component: UpdateComponent},
+
+
+    {
+      path: '**',
+      component: AccueilComponent}
+     ] ;
 
 
 @NgModule({
@@ -13,10 +51,16 @@ import { HeaderComponent } from 'src/app/header/header.component';
     AppComponent,
     HeaderComponent,
     BodyComponent,
-    FooterComponent
+    FooterComponent,
+    AccueilComponent,
+    SearchComponent,
+    CreateComponent,
+    UpdateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
