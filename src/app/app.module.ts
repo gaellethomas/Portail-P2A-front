6a5862a1d78cd9723d49919a75a3b84c064c5751
unknown <gaelle.thomas.pf1@gmail.com>
2018-10-30 00:33:29 +0100

    update open links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -60,7 +60,7 @@ const routes: Routes = [
     ResultComponent,
     DataListsComponent,
     LinkListComponent,
-    PersonListComponent,
+    PersonListComponent
   ],
   imports: [
     BrowserModule,
